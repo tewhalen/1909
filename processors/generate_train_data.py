9d9354497d6a2bb82e418cfe62d58f73abae7745
ted whalen <tewhalen@gmail.com>
2021-11-14 22:14:00 -0600

    add in the 10% most-confident data to the extract to ensure we're not only training on the bad stuff

--- processors/generate_train_data.py
+++ processors/generate_train_data.py
@@ -19,6 +19,7 @@ FILTER_OUT = ["|"]
 # i know this is bad but i'm doing it anyway
 TESSDATADIR = "../../"
 
+
 def filter_horiz_lines(text: str):
     "returns true if this is not just horizontal line stuff"
 
@@ -34,15 +35,17 @@ def prepare_ocr_data(filename: str) -> pd.DataFrame:
     ocr_data = pytesseract.image_to_data(
         Image.open(filename),
         output_type=pytesseract.Output.DATAFRAME,
-        config="--tessdata-dir {} -l 1909 --psm 6".format(TESSDATADIR),  # Assume a single uniform block of text.
+        config="--tessdata-dir {} -l 1909 --psm 6".format(
+            TESSDATADIR
+        ),  # Assume a single uniform block of text.
     ).dropna()
 
     # we assume the mode is the basic row, and throw out everything that's
     # three times the height of that
     height_mode = ocr_data["height"].mode()[0]  # mode is a series, just use the top
-   
+
     ocr_data = ocr_data[ocr_data["height"] <= height_mode * 3]
-    
+
     # and lets throw out anything that's less than 75% the height of the regular row
     ocr_data = ocr_data[ocr_data["height"] > height_mode * 0.75]
 
@@ -57,37 +60,39 @@ def prepare_ocr_data(filename: str) -> pd.DataFrame:
     return ocr_data
 
 
-
 @click.command()
 @click.argument("filename", type=click.Path(exists=True))
 @click.argument("prefix", type=str)
 def make_training_data(filename, prefix):
-    """OCR the file, find the segments that are either 
+    """OCR the file, find the segments that are either
     a) more than a standard deviation under the mean condfidence level; or
     b) aren't entirely numeric
-    
+
     and write out the image segment as png, along with the text the OCR produced
-    
+
     This is meant to be hand-corrected and used to train the LSTM model"""
 
     ocr_data = prepare_ocr_data(filename)
     base_img = Image.open(filename)
 
-    mean_conf = ocr_data['conf'].mean()
-    sd_conf = ocr_data['conf'].std()
+    mean_conf = ocr_data["conf"].mean()
+    sd_conf = ocr_data["conf"].std()
 
-    print("median confidence:",mean_conf, sd_conf)
+    # leaven in the top 10% most-confident
+    best_q = ocr_data["conf"].quantile(0.9)
+
+    print("mean confidence: {:.02f} {:.02f}".format(mean_conf, sd_conf))
 
     for index, row in ocr_data.iterrows():
 
-        if (row.conf < (mean_conf - sd_conf)) or not row.text.isnumeric():
+        if (row.conf > best_q) or (
+            (row.conf < (mean_conf - sd_conf)) or not row.text.isnumeric()
+        ):
             # output a cropped image
             output_fn = "{}-{:05}.png".format(prefix, index)
             box = base_img.crop((row.left, row.top, row.right, row.bot))
             box.save(output_fn)
-            open("{}-{:05n}.gt.txt".format(prefix, index),'w').write(row.text)
-
-
+            open("{}-{:05n}.gt.txt".format(prefix, index), "w").write(row.text)
 
 
 if __name__ == "__main__":
